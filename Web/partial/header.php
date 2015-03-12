d65a8f5386b6014ccf14391e9a0abe4398915c37
wild006 <wild006@hotmail.com>
2015-03-12 16:33:36 -0400

    Ajout header

--- Web/partial/header.php
+++ Web/partial/header.php
@@ -1,11 +1,51 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<!DOCTYPE html>
+<!-- saved from url=(0023)http://muralecatsa.com/ -->
+<html class="wf-myriadpro-n3-active wf-myriadpro-n4-active wf-myriadpro-n6-active wf-myriadpro-n7-active wf-myriadpro-n9-active wf-active">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Catsa.</title>
-    <link href="css/global.css" rel="stylesheet" type="text/css" media="screen" />
-    <link href="css/jquery.lightbox-0.5.css" rel="stylesheet" type="text/css" media="screen" />
-    <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
-    <script type="text/javascript" src="js/jquery.lightbox-0.5.min.js"></script>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- <link rel="stylesheet" type="text/css" href="./css/catsa-screen.css"> -->
+    <link rel="shortcut icon" href="http://muralecatsa.com/favicon.png">
+    <meta property="og:image" content="/tableaux/survol/noir_et_blanc.jpg">
+    <title>Murales Catsa, Chambres et commerces à Montréal, Laval, Rive-Sud et les environs</title>
+
+    <link type="text/css" href="./catsa_files/pikachoose-classic-theme.css" rel="stylesheet">
+    <script type="text/javascript" async="" src="./catsa_files/ga.js"></script>
+    <script src="./catsa_files/krk5ozc.js" type="text/javascript" async=""></script>
+    <script type="text/javascript" src="./catsa_files/jquery.min.js"></script>
+    <script type="text/javascript" src="js/javascript.js"></script>
+    <script type="text/javascript" src="./catsa_files/jquery.pikachoose.full.js"></script>
+
+    <!-- Add mousewheel plugin (this is optional) -->
+    <script type="text/javascript" src="./catsa_files/jquery.mousewheel-3.0.6.pack.js"></script>
+
+    <!-- Add fancyBox -->
+    <link rel="stylesheet" href="./catsa_files/jquery.fancybox.css" type="text/css" media="screen">
+    <script type="text/javascript" src="./catsa_files/jquery.fancybox.pack.js"></script>
+
+    <!-- Optionally add helpers - button, thumbnail and/or media -->
+    <link rel="stylesheet" href="./catsa_files/jquery.fancybox-buttons.css" type="text/css" media="screen">
+    <script type="text/javascript" src="./catsa_files/jquery.fancybox-buttons.js"></script>
+    <script type="text/javascript" src="./catsa_files/jquery.fancybox-media.js"></script>
+
+    <link rel="stylesheet" href="./catsa_files/jquery.fancybox-thumbs.css" type="text/css" media="screen">
+    <script type="text/javascript" src="./catsa_files/jquery.fancybox-thumbs.js"></script>
+
+    <style type="text/css">.tk-myriad-pro{font-family:"myriad-pro",sans-serif;}</style>
+    <link rel="stylesheet" href="http://use.typekit.net/c/3df090/1w;myriad-pro,2,Xc9:F:n3,XcH:F:n4,XcJ:F:n6,Xc0:F:n7,Xbt:F:n9/d?3bb2a6e53c9684ffdc9a9bf6195b2a62173cbd03ca8deaa4c4329ac79a481f20c10c1b9844e45de6fbbacf8ec095e3bcb4980b25aa05d5a20e9fd065d9c5df18f5e2027ef79623ea62647b4186e1e284cb57f8e99f28e73607b1c4fbe14c44a2245ccd20788e2d">
   </head>
-  <body>
+  <body class="" style="margin-right: 0px;">
+
+    <div style="width:850px;margin:auto;">
+      <div style="height:100px;font-size:18px;">
+        <div style="padding-top:120px;float:left;color:#333;">
+        	<ul>
+	          <img style="position:absolute;margin-top:-90px;z-index:-5;" src="./catsa_files/coc1.jpg"><li><a href=""><b>murales</b></a></li>
+	          <li><a href="catsatableau.html">tableaux</a></li>
+	          <li><a href="catsanous.html">nous</a></li>
+          	</ul>
+        </div>
+        <div style="padding-top:70px;float:right;font-family:&#39;myriad-pro&#39;;font-weight:100;font-size:68px;z-index:10;color:#555;">catsa</div>
+      </div>
+      <div style="position:absolute;z-index:-100;">
+        <img src="./catsa_files/cocinelles.jpg" id="coc" style="position:relative;left:790px;top:-100px;z-index:-10;">
+      </div>
