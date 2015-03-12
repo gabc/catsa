71e55993f08617aed56eab61bb3f1288382f1f53
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-03-12 14:55:26 -0400

    Population de login.php avec le squelette

--- Web/login.php
+++ Web/login.php
@@ -1,1 +1,10 @@
-login.php
+<?php
+	require_once("action/LoginAction.php");
+
+	$action = new LoginAction();
+	$action->execute();
+
+	require_once("partial/header.php")
+?>
+	<p> Fooo </p>
+<?php require_once("partial/footer.php");
