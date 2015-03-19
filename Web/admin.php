1d4e67e67380205c31fa45ff6d6ead870bbfbbba
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-03-19 15:31:31 +0100

    ckeditor dans l'admin

--- Web/admin.php
+++ Web/admin.php
@@ -7,4 +7,15 @@
 	require_once("partial/header.php")
 ?>
 	<p> Fooo </p>
+
+	<script src="js/ckeditor/ckeditor.js"></script>
+	<textarea cols="80" id="editor1" name="editor1" rows="10" >
+	</textarea>
+
+	<script>
+		CKEDITOR.replace( 'editor1', {
+			height: 260
+		} );
+	</script>
+
 <?php require_once("partial/footer.php");
