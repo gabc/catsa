bc6850f1920b8c33ae44c59142d42dc55d1594be
wild006 <wild006@hotmail.com>
2015-03-21 08:37:07 -0400

    Ajout fancybox

--- Web/partial/header.php
+++ Web/partial/header.php
@@ -12,24 +12,24 @@
     <link type="text/css" href="./css/global.css" rel="stylesheet">
     <script type="text/javascript" async="" src="./catsa_files/ga.js"></script>
     <script src="./catsa_files/krk5ozc.js" type="text/javascript" async=""></script>
-    <script type="text/javascript" src="./catsa_files/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/jquery.min.js"></script>
     <script type="text/javascript" src="./catsa_files/jquery.pikachoose.full.js"></script>
     <script type="text/javascript" src="js/javascript.js"></script>
 
     <!-- Add mousewheel plugin (this is optional) -->
-    <script type="text/javascript" src="./catsa_files/jquery.mousewheel-3.0.6.pack.js"></script>
+    <script type="text/javascript" src="./js/fancyBox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
 
     <!-- Add fancyBox -->
-    <link rel="stylesheet" href="./catsa_files/jquery.fancybox.css" type="text/css" media="screen">
-    <script type="text/javascript" src="./catsa_files/jquery.fancybox.pack.js"></script>
+    <link rel="stylesheet" href="./js/fancyBox/source/jquery.fancybox.css" type="text/css" media="screen">
+    <script type="text/javascript" src="./js/fancyBox/source/jquery.fancybox.pack.js"></script>
 
     <!-- Optionally add helpers - button, thumbnail and/or media -->
-    <link rel="stylesheet" href="./catsa_files/jquery.fancybox-buttons.css" type="text/css" media="screen">
-    <script type="text/javascript" src="./catsa_files/jquery.fancybox-buttons.js"></script>
-    <script type="text/javascript" src="./catsa_files/jquery.fancybox-media.js"></script>
+    <link rel="stylesheet" href="./js/fancyBox/source/helpers/jquery.fancybox-buttons.css" type="text/css" media="screen">
+    <script type="text/javascript" src="./js/fancyBox/source/helpers/jquery.fancybox-buttons.js"></script>
+    <script type="text/javascript" src="./js/fancyBox/source/helpers/jquery.fancybox-media.js"></script>
 
-    <link rel="stylesheet" href="./catsa_files/jquery.fancybox-thumbs.css" type="text/css" media="screen">
-    <script type="text/javascript" src="./catsa_files/jquery.fancybox-thumbs.js"></script>
+    <link rel="stylesheet" href="./js/fancyBox/source/helpers/jquery.fancybox-thumbs.css" type="text/css" media="screen">
+    <script type="text/javascript" src="./js/fancyBox/source/helpers/jquery.fancybox-thumbs.js"></script>
   </head>
   <body class="" style="margin-right: 0px;">
 
