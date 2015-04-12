0b7ec3611f5c8861a260de9ec3a087ba5a234384
wild006 <wild006@hotmail.com>
2015-04-12 17:33:13 -0400

    Changement look login

--- Web/login.php
+++ Web/login.php
@@ -6,18 +6,20 @@
 
 	require_once("partial/header_admin.php")
 ?>
+	<div class="center">
+		<h2>Admin</h2>
+		<?php 
+			if ($action->wrongLogin) {
+		?>
+				<div class="error-div"><strong>Erreur : </strong>Connexion erronée</div>
+		<?php
+			}
+		?>
 
-	<?php 
-		if ($action->wrongLogin) {
-	?>
-			<div class="error-div"><strong>Erreur : </strong>Connexion erronée</div>
-	<?php
-		}
-	?>
-
-	<form method="post" action="login.php">
-	    	<p><input type="text" name="username" value="" placeholder="Nom d'utilisateur"></p>
-	    	<p><input type="password" name="pwd" value="" placeholder="Mot de passe"></p>
-			<button type="submit">Login</button>
-	</form>
+		<form method="post" action="login.php">
+		    	<p><input type="text" name="username" value="" placeholder="Nom d'utilisateur"></p>
+		    	<p><input type="password" name="pwd" value="" placeholder="Mot de passe"></p>
+				<button type="submit">Login</button>
+		</form>
+	</div>
 <?php require_once("partial/footer_admin.php");
