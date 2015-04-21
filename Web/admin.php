86f310e31fbe0794ec25305400d0fc7ed4c00ab2
wild006 <wild006@hotmail.com>
2015-04-21 15:49:26 -0400

    Changement visuel text editor admin

--- Web/admin.php
+++ Web/admin.php
@@ -6,27 +6,30 @@
 
 	require_once("partial/header_admin.php")
 ?>
+  
+  <div class="center">
+    <form method="post" action="admin.php">
+  	<script src="js/ckeditor/ckeditor.js"></script>
 
-        <form method="post" action="admin.php">
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-	  	<script src="js/ckeditor/ckeditor.js"></script>
-
-<div id="tabs">
-  <ul>
-    <li><a href="#tab" id="acceuil">Acceuil</a></li>
-    <li><a href="#tabs-2" id="contact">Contact</a></li>
-    <li><a href="#tabs-3" id="presentation">Presentation</a></li>
-  </ul>
-  <div id="tabs-1">
-   </div>
-  <div id="tabs-2">
+    <div id="tabs">
+      <ul>
+        <li><a href="#tabs-1" id="acceuil">Acceuil</a></li>
+        <li><a href="#tabs-2" id="contact">Contact</a></li>
+        <li><a href="#tabs-3" id="presentation">Presentation</a></li>
+      </ul>
+      <textarea cols="80" id="editor1" name="editor1" rows="10" >
+        </textarea>
+      <button type="submit">Nouveau texte</button>
+      <div id="tabs-1">
+       </div>
+      <div id="tabs-2">
+      </div>
+      <div id="tabs-3">
+      </div>
+    </div>
+    		
+        
+    </form>
   </div>
-  <div id="tabs-3">
-  </div>
-</div>
-		<textarea cols="80" id="editor1" name="editor1" rows="10" >
-		</textarea>
-        	<button type="submit">Nouveau texte</button>
-        </form>
 
 <?php require_once("partial/footer_admin.php");
