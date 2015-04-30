b95966d0abab7e622d47b1a206c385ffac991f0d
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-04-30 14:15:23 -0400

    Fixup de la gestion de mot de passe

--- Web/action/DAO/UserDAO.php
+++ Web/action/DAO/UserDAO.php
@@ -1,35 +1,27 @@
 <?php 
+	require_once("lib/password.php");
+
 	class UserDAO {
-		require_once("lib/password.php");
+
 		public static function authenticate($username, $pwd) {
-			// connexion à la BD, fichier texte, serveur externe, ...
 			$visibility = CommonAction::$VISIBILITY_PUBLIC;
 
-			// if ($username == "a" && $pwd == "a") {
-			// 	$visibility = CommonAction::$VISIBILITY_MEMBER;
-			// }
-
-			// return $visibility;
 			$connection = Connection::getConnection();
-			
-			$pwd = password_hash($pwd, PASSWORD_DEFAULT);
+
 			$statement = $connection->prepare("SELECT * FROM CS_USER WHERE USERNAME = ?");
 			$statement->bindParam(1, $username);
 
 			$statement->setFetchMode(PDO::FETCH_ASSOC);
 			$statement->execute();
 
-			// connexion à la BD, fichier texte, serveur externe, ...
 			$user = null;
 
 			if ($row = $statement->fetch()) {
-				if (password_verify($pwd, $row["PASSWORD"])) {
+				if (password_verify($pwd, $row["MOTDEPASSE"])) {
 					$user = $row;
-					unset($user["PASSWORD"]);
+					unset($user["MOTDEPASSE"]);
 				}
 			}
-
 			return $user;
 		}
-
 	}
