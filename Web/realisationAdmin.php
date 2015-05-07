1057acaa0608fc423a4aa1f98e8b1a42feafabd5
wild006 <wild006@hotmail.com>
2015-05-07 17:05:50 -0400

    Ajout bootstrap modal + stack

--- Web/realisationAdmin.php
+++ Web/realisationAdmin.php
@@ -24,7 +24,7 @@
 		  					<img class="col-md-2 img-rounded" src="<?= $action->creations[$i]["IMAGE"] ?>">
 		    				<p class="col-md-7"><?= $action->creations[$i]["DESCRIPTION"] ?></p>
 		    				<span class="col-md-1"><input disabled type="checkbox" name="slideshow" value="slideshow" <?= ($action->creations[$i]["SLIDESHOW"]) ? "checked" : "" ;?> >Dans le slideshow</span>
-		    				<a class="btn col-md-1" href="#" role="button"><span class="glyphicon glyphicon-pencil"></span></a>
+		    				<a class="btn col-md-1" data-toggle="modal" href="#stack1" role="button"><span class="glyphicon glyphicon-pencil"></span></a>
 		    				<a class="btn col-md-1" href="#" role="button"><span class="glyphicon glyphicon-remove"></span></a>
 		    			</div>
 		    		</div>
@@ -55,4 +55,37 @@
 	    }
 	   	?>
 	   	</ul>
+	   	<link type="text/css" href="css/bootstrap_patch.css" rel="stylesheet">
+		<link type="text/css" href="css/bootstrap-modal.css" rel="stylesheet">
+<script type="text/javascript" src="js/bootstrap-modal.js"></script>
+<script type="text/javascript" src="js/bootstrap-modalmanager.js"></script>
+
+	<!-- Dialog - Ajouter une catégorie -->
+    <div id="stack1" class="modal fade bs-modal-lg" tabindex="-1" style="display: none;" role="dialog" data-focus-on="input:first" aria-labelledby="creationModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+        	<form class="form-horizontal" action='realisationAdmin.php' method="POST">
+	   			<fieldset>
+	            	<div class="modal-header">
+	                	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	                	<legend class="modal-title">Modifier une création</legend>
+	              	</div>
+	              	<div class="modal-body">
+	              	<script type="text/javascript" src="js/adminRealisation.js"></script>
+	              		 <?php
+							  require_once("action/ModifRealisationAdminAction.php");
+								
+							  $action = new ModifRealisationAdminAction();
+							  $action->execute();
+							  require_once("creationForm.php");
+							?>
+							
+	   					 <button type="submit" class="btn btn-default btn-lg btn-block">Modifier</button>
+	   				</div>
+            	</fieldset>
+            </form>
+	   	</div>
+	   </div>
+    </div>
+
 <?php require_once("partial/footer_admin.php");
