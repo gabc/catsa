9113fbc30f03f9b3bc324e4e86ff61025cd0488c
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-05-12 14:00:24 -0400

    Fix pour la colorisation du lien.
        Ca devrait pas chier.
        Mais c'est pas super clean..

--- Web/js/admin.js
+++ Web/js/admin.js
@@ -3,8 +3,8 @@ $(init);
 function init () {
     activationMenu();
     try{
-	    var href = window.location.href.match(/.*\/(?!.*admin.php)(.*\.php)$/)[1];
-	    $("a[href='"+ href +"']").css("background-color", "black");
+	    var href = window.location.href.match(/.*\/(?!.*admin.php)(.*\.php)/)[1];
+	    $("a[href*='"+ href +"']").css("background-color", "black");
 	}catch (e){
 		// Gotta catch 'em all!
 	}
