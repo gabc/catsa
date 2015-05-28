ae553a40e4032b232f9548ce26880194544b11db
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-05-28 13:45:58 -0400

    Berk * 100

--- Web/js/admin.js
+++ Web/js/admin.js
@@ -3,8 +3,13 @@ $(init);
 function init () {
     activationMenu();
     try{
-	    var href = window.location.href.match(/.*\/(?!.*admin.php)(.*\.php)/)[1];
-	    $("a[href*='"+ href +"']").css("background-color", "#FAFFBA");
+    	var reg = window.location.href.match(/.*\/(?!.*admin.php)(.*\.php)(?:.*item=(real|news))?/);
+	    // var href = window.location.href.match(/.*\/(?!.*admin.php)(.*\.php)item=(.*)/)[1];
+	    if(reg[2] === undefined) {
+	    	$("a[href*='"+ reg[1] +"']").css("background-color", "#FAFFBA");
+	    } else {
+	    	$("a[href*='"+ reg[1] + "?page=1&item=" + reg[2] +"']").css("background-color", "#FAFFBA");
+	    }
 	}catch (e){
 		// Gotta catch 'em all!
 	}
