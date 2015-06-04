e712944ff5c66dadf8fb6cb6661680cc34025a2b
wild006 <wild006@hotmail.com>
2015-06-04 12:33:47 -0400

    fix insert

--- DB/insert_test.sql
+++ DB/insert_test.sql
@@ -1,16 +1,15 @@
-/*INSERT TEST*/
-
+/*INSERT*/
 INSERT INTO CS_User(USERNAME, MOTDEPASSE, VISIBILITY)
 VALUES('admin', '$2a$10$FuPMKanOvLxrrEXGbzAM1u.r63nbZfUOY2ysjG7dnLMyLZDa2aUmK', '2');
 
 INSERT INTO CS_TEXTE(EMPLACEMENT, CONTENU)
-VALUES('acceuil', 'Bienvenue sur Catsa !');
+VALUES('acceuil', 'Bienvenue sur Catsa ! Nous faisons des murales pour les gens près de Montréal, Laval, Rive-Sud et les environs ! Depuis 2003, nous faisons des peintures murales hautes gammes ainsi que de magnifiques tableaux !');
 
 INSERT INTO CS_TEXTE(EMPLACEMENT, CONTENU)
-VALUES('contact', 'Contact');
+VALUES('contact', 'Contactez-moi par téléphone ou par courriel pour toutes informations supplémentaires, faire évaluer la valeur de votre projet et pour prendre rendez-vous.');
 
 INSERT INTO CS_TEXTE(EMPLACEMENT, CONTENU)
-VALUES('presentation', 'Qui suis-je ?');
+VALUES('presentation', 'En 2003, Sarah Bronsard et moi unissons nos noms, notre talent et notre grande passion pour la peinture en démarrant catsa, une entreprise de peinture murale haut de gamme.');
 
 INSERT INTO CS_TYPE(NOM)
 VALUES('Chambre');
