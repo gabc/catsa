0caf20f83e35b741c9b5450e279746ccc38bdb31
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-06-04 11:47:08 -0400

    MOAR FIXES!

--- README.txt
+++ README.txt
@@ -1,8 +1,6 @@
 !!!!!!Très important ! Ce fichier contiendra les informations relatives à l’installation et au fonctionnement du site Web.!!!!!!!!!!!!!!
 
 
-Il possèdera les informations de connexion (nom de la page Web/nom usager/mot de passe) permettant d’accéder au gestionnaire de contenu du site Web. Finalement, le fichier devra contenir un court texte explicatif de ce que vous avez fait pour optimiser le référencement du site Web (environ 250 mots). Spécifier vos sources (URL).
-
 * Utilisation inteligente/strategique des balises
  - H1
    Pas plus d'un par page.
--- Web/js/admin.js
+++ Web/js/admin.js
@@ -6,7 +6,13 @@ function init () {
     	var reg = window.location.href.match(/Web\/((?:.*)admin)(?:\/(real|news))?/i);
 	    // var href = window.location.href.match(/.*\/(?!.*admin.php)(.*\.php)item=(.*)/)[1];
 	    if(reg[2] === undefined) {
-	    	$("a[href*='"+ reg[1] +"']").css("background-color", "#FAFFBA");
+	    	var a = $("a[href*='"+ reg[1] +"']");
+	    	for(var i = 0; i < a.length; i++){
+	    		if(a[i].className.match(/adminLink/)){
+	    			a[i].style.backgroundColor = "#FAFFBA"
+	    		}
+	    	}
+	    	// $("a[href*='"+ reg[1] +"']").css("background-color", "#FAFFBA");
 	    } else {
 	    	var a = $("a[href*='"+ reg[1] + "/" + reg[2] +"/1']");
 	    	for(var i = 0; i < a.length; i++){
